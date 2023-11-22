82c28f2608342720010226ffc52279143e94d22c
DungxLas <phamjhungfdungx@gmail.com>
2023-11-22 16:25:35 +0700

    Update chat.dart

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,36 @@
-import 'package:calendar_app/widgets/chat_messages.dart';
-import 'package:calendar_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:calendar_app/widgets/chat_messages.dart';
+import 'package:calendar_app/widgets/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    // final token = await fcm.getToken();
+    // print(
+    //     token); // you could send this token (via HTTP or the Firebstore SDK) to a backend
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
